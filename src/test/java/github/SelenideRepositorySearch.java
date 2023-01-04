97da28fbd1b2d7fc4b81eef0cb040ebb4ac8bc91
alekseibulatov <59173362+alekseibulatov@users.noreply.github.com>
2023-01-05 00:06:07 +0300

    add  SelenideRepositorySearch

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,5 +1,22 @@
-package java.github;
+package github;
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class SelenideRepositorySearch {
-
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+        // открыть сайт github.com
+        open("https://github.com/");
+        // ввести в поле поиска selenide и нажать enter
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        // кликнуть на первый репозиторий в из списка найденых
+        $$("ul.repo-list li").first().$("a").click();
+        // проверка: заголовок selenide/ selenide
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+    }
 }
